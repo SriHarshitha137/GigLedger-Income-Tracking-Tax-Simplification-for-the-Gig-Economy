55d2df3d3c0ba791df308a0989a164695fcc77d3
fproject <fproject@gmail.in>
2026-04-29 11:48:15 +0530

    Deploying

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: 'https://gig-ledger-income-tracking-tax-simp.vercel.app',
     credentials: true
   })
 );
